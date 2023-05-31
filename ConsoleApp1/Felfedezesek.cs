5b8f9af296e9506289622895fb7f4bc2793863da
tanulo <tanulo@4676-7>
2023-05-31 09:34:05 +0200

    Add project files.

---
+++ ConsoleApp1/Felfedezesek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Felfedezesek
+    {
+        string ev;
+        string nev;
+        string vegyjel;
+        int rendszam;
+        string felfedezo;
+
+        public Felfedezesek(string ev, string nev, string vegyjel, int rendszam, string felfedezo)
+        {
+            this.ev = ev;
+            this.nev = nev;
+            this.vegyjel = vegyjel;
+            this.rendszam = rendszam;
+            this.felfedezo = felfedezo;
+        }
+
+        public string Ev { get => ev; set => ev = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public string Vegyjel { get => vegyjel; set => vegyjel = value; }
+        public int Rendszam { get => rendszam; set => rendszam = value; }
+        public string Felfedezo { get => felfedezo; set => felfedezo = value; }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Felfedezesek> f = new List<Felfedezesek>();
+
+            StreamReader sr = new StreamReader("felfedezesek.csv");
+            sr.ReadLine();
+            while (!sr.EndOfStream)
+            {
+                string line = sr.ReadLine();
+                string[] splitLine = line.Split(';');
+
+                f.Add(new Felfedezesek(splitLine[0], splitLine[1], splitLine[2], Int32.Parse(splitLine[3]), splitLine[4]));
+            }
+
+            if (f.Count > 200)
+            {
+                Console.WriteLine("Túl hosszú a bemeneti adatfájl");
+                System.Environment.Exit(-1);
+            } else
+            {
+                Console.WriteLine($"3. feladat: Elemek száma: {f.Count()}");
+            }
+
+            int okoriFelfedezesek = 0;
+
+            okoriFelfedezesek = f.FindAll(x => x.Ev == "Ókor").Count;
+
+            Console.WriteLine($"4. feladat: Felfedezések száma az ókorban: {okoriFelfedezesek}");
+
+            bool found = false;
+            string input = "";
+            Felfedezesek foundItem;
+            while (!found)
+            {
+                Console.Write("5. feladat: Kérek egy vegyjelet:");
+                input = Console.ReadLine().ToLower();
+                Console.Write("\n");
+                if (input.Length == 1 || input.Length == 2)
+                {
+                    found = true;
+                }
+            }
+
+            if (f.Exists(x => x.Vegyjel.ToLower() == input))
+            {
+                foundItem = f.Find(x => x.Vegyjel.ToLower() == input);
+                Console.WriteLine($"6. feladat: Keresés\n\t Az elem vegyjele: {foundItem.Vegyjel}\n\t Az elem neve: {foundItem.Nev}\n\t Rendszáma: {foundItem.Rendszam}\n\t Felfedezés éve: {foundItem.Ev}\n\t Felfedező: {foundItem.Felfedezo}");
+            } else
+            {
+                Console.WriteLine("Nincs ilyen elem az adatforrásban");
+            }
+
+            int highestInterval = 0;
+
+            foreach (Felfedezesek felfedezes in f)
+            {
+
+            }
+
+        }
+    }
+}
